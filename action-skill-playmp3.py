320743de5fed56c06cc196ccae22e9553e01c6dc
Rob Hedrick <rob@hedrickholdings.com>
2019-11-04 19:49:57 -0600

    add tracks

--- action-skill-playmp3.py
+++ action-skill-playmp3.py
@@ -5,8 +5,6 @@ from hermes_python.hermes import Hermes
 from hermes_python.ontology import *
 import io
 import subprocess
-#from subprocess import run
-#from playsound import playsound
 
 CONFIG_INI = "config.ini"
 
@@ -14,8 +12,6 @@ MQTT_IP_ADDR: str = "localhost"
 MQTT_PORT: int = 1883
 MQTT_ADDR: str = "{}:{}".format(MQTT_IP_ADDR, str(MQTT_PORT))
 def play_mp3(path):
-    #subprocess.run.(['playsound', path])
-    #subprocess.call.(['playsound', path])
     subprocess.Popen(['mpg123', '-q', path]).wait()
 
 class playMP3(object):
@@ -31,8 +27,16 @@ class playMP3(object):
     def execplaymp3_callback(self, hermes, intent_message):
 
         if intent_message.slots.playmp3:
-            play_mp3('/media/turndownforwhat.mp3')
-            hermes.publish_end_session(intent_message.session_id, "") 
+            intent=intent_message.slots.playmp3.first().value
+            if intent == "four what" or intent == "for what" or intent == "fo what":
+                play_mp3('/media/turndownforwhat.mp3')
+                hermes.publish_end_session(intent_message.session_id, "")
+            elif intent == "goodbye" or intent == "good bye" or intent == "good by":
+                play_mp3('/media/byenow.mp3')
+                hermes.publish_end_session(intent_message.session_id, "")
+            elif intent == "congratulate" or intent == "congratulations":
+                play_mp3('/media/ohgoodforyou.mp3')
+                hermes.publish_end_session(intent_message.session_id, "")
 
     def master_intent_callback(self,hermes, intent_message):
         coming_intent = intent_message.intent.intent_name
